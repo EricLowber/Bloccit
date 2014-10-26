d6e4c4721dab8802494749b165dd85be2f4e43bc
vagrant <vagrant@rails-dev-box>
2014-10-26 14:20:37 +0000

    Associated and nested topics and posts,

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,25 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
   end
 
   def new
-    @post = Post.new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new    
     authorize @post
   end
 
   def create
+     @topic = Topic.find(params[:topic_id])
      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+     @post.topic = @topic
+     authorize @post
       if @post.save
        flash[:notice] = "Post was saved."
-        redirect_to @post
+        redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -25,16 +27,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
+    authorize @post
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       authorize @post
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There ws an error saving the post. Please try again."
       render :edit
