55a52bb1968c27e20e556a0f6cb63f8c2f2a2d88
vagrant <vagrant@rails-dev-box>
2014-11-06 02:55:10 +0000

    added deleting functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Bloccit::Application.routes.draw do
   
   get "comments/create"
   devise_for :users
-   resources :users, only: [:update]
-resources :topics do 
-  resources :posts, except: [:index] 
+  resources :topics do
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
     end
-
-resources :posts, only: [] do
-  resources :comments, only: [:create, :destroy]
-end
+  end
 
 
+   resources :topics do
+     resources :posts, except: [:index]
+   end
   get  'about' => 'welcome#about'
 
 
