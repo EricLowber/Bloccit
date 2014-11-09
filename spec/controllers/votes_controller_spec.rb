6b111ad75e67dd26e0bd24c05e951ad06eb90a8a
vagrant <vagrant@rails-dev-box>
2014-11-09 01:05:03 +0000

    added down-vote spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,16 +4,26 @@ describe VotesController  do
   include TestFactories
   include Devise::TestHelpers
 
+before do
+      request.env["HTTP_REFERER"] = '/'
+      sign_in @user
+      @user = authenticated_user
+      @post = associated_post     
+end
+
   describe '#up_vote' do 
     it "adds an up-vote to the post" do
-      @user = authenticated_user
-      @post = associated_post
-      sign_in @user
-            request.env["HTTP_REFERER"] = '/'
-
       expect {
         post( :up_vote, post_id: @post.id )
         }.to change{ @post.up_votes }.by 1
       end
    end
-end
+
+   describe '#down_vote' do
+    it "adds a down-vote to the post" do
+      expect {
+        post( :down_vote, post_id: @post.id )
+        }.to change{ @post.down_votes }.by 1
+      end
+    end
+  end
