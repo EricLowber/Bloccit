22096de5a3d9a731e78dd09097e10403f3d707c0
vagrant <vagrant@rails-dev-box>
2014-11-12 01:13:50 +0000

    defined publicly and privatly viewable scopes on Topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,11 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  scope :visible_to, -> (user) { user ? all : public }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
 
+  scope :public, -> { where(public: true) }
+  scope :private, -> { where(public: false) }
+  scope :visible_to, -> (user) { user ? all : public }
 end
