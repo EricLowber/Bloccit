157156fd74a64d19d82a5aa318d0924bf45cc9d2
vagrant <vagrant@rails-dev-box>
2014-11-21 00:58:29 +0000

    added popular posts and users

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -11,10 +11,8 @@ class TopicsController < ApplicationController
 
   def show 
     @topic = Topic.find(params[:id])
-    @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
     authorize @topic
-
-    puts 'hello'
+    @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
   end
 
   def edit
